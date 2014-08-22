71e9cf2f81600305c43f23ba3839336b432f663e
Josh Wardini <joshwardini@gmail.com>
2014-08-22 12:11:16 -0700

    manual fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+numEnd = 100
+toPrint = " "
+
+for i in range(1,numEnd+1):
+    if i%3 == 0 and i%5 == 0:
+        print "fizz buzz"
+    elif i%3 == 0:
+        print "fizz"
+    elif i%5 == 0:
+        print "buzz"
+    else:
+        print i
