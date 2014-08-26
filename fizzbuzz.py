3fbeba45bcde787f97c01a2f1baec3751a356b47
Josh Wardini <joshwardini@gmail.com>
2014-08-25 17:12:39 -0700

    Command input working, raw_input not working

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,31 @@
 import sys
 
+#tests a division to see if the numbers have 0 remainder
+# returns true or false
+def evenDivis(num1,divNum):
+    if num1%divNum == 0:
+        return True
+    else:
+        return False
+
+# places fizz in place of all multiples of 3, places buzz in all multiples of 5
+# places fizz buzz whenever the input number is a multipile of 3 and 5
 def fizzBuzz(numEnd):
     for i in range(1,int(numEnd)+1):
-        if i%3 == 0 and i%5 == 0:
+        if evenDivis(i,3) and evenDivis(i,5):
             print "fizz buzz"
-        elif i%3 == 0:
+        elif evenDivis(i,3):
             print "fizz"
-        elif i%5 == 0:
+        elif evenDivis(i,5):
             print "buzz"
         else:
             print i
 
-if len(sys.argv) >= 2:
+if __name__ == '__main__':
     fizzBuzz(sys.argv[1])
+
 else:
-    inputNum = int(raw_input("Enter something, yo! "))
-    fizzBuzz(inputNum)
+    rawInput = fizzBuzz(raw_input("enter your digits : "))
+    print rawInput
 
 
